37ae0479cb9172d6c60722ca6035f9b31e955fc6
Tapoban Ray <raytapoban@gmail.com>
2025-03-25 17:57:48 +0530

    Handled secrets with dotenv

--- todo-app/database.py
+++ todo-app/database.py
@@ -1,11 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from typing import Annotated
 from dotenv import load_dotenv
 import os
 
-DATEBASE_URL = f"postgresql+psycopg2://postgres:tapoban@localhost:5432/to-do_appDB"
+load_dotenv()
+
+USERNAME = os.environ.get("DB_USERNAME")
+PASSWORD = os.environ.get("DB_PASSWORD")
+DATABASE_NAME = os.environ.get("DATABASE_NAME")
+
+print(USERNAME, PASSWORD, DATABASE_NAME)
+
+DATEBASE_URL = (
+    f"postgresql+psycopg2://{USERNAME}:{PASSWORD}@localhost:5432/{DATABASE_NAME}"
+)
 
 engine = create_engine(DATEBASE_URL)
 
